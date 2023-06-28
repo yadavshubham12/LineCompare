44adede0b598f1ad6f51b6d66d7bde961397b11f
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-06-28 23:36:00 +0530

    ADDED: Welcome to line comparison program

---
+++ src/com/blz/lineComparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.blz.lineComparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line Comparison Program");
+    }
+}
